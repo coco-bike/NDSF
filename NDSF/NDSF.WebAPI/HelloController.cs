4d1fe595eb878b4c1e3f810d425f154b8fe62178
必可 宋 <444503829@qq.com>
2022-05-28 16:55:43 +0800

    添加Dapr容器

---
+++ NDSF/NDSF.WebAPI/HelloController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NDSF.WebAPI
+{
+    [ApiController]
+    public class HelloController : ControllerBase
+    {
+        [HttpGet("api/Hello")]
+        public async Task<IActionResult> Hello()
+        {
+            await Task.CompletedTask;
+
+            return Ok("Hello Dapr");
+        }
+    }
+}
